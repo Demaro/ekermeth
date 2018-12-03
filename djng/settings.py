7c4c1619e16da64bcb5f7a86992954cd1d48e1e2
demaro <demaromail@gmail.com>
2018-12-03 12:18:57 -0300

    design resumen

--- djng/settings.py
+++ djng/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'djng.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'static/ang'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
