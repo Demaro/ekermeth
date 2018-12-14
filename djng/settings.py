d9088bbfdbe790de87c860b6661d74bef169ccff
demaro <demaromail@gmail.com>
2018-12-14 13:34:20 -0300

    cors rest

--- djng/settings.py
+++ djng/settings.py
@@ -133,6 +133,13 @@ CORS_ORIGIN_WHITELIST = (
 
 )
 
+ALLOWED_HOSTS = [
+'https://test-ekermet.herokuapp.com/',
+
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_REGEX_WHITELIST = (r'^(https?://)?(\w+\.)?google\.com$', )
